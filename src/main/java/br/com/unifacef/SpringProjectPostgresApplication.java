cd968f5fb524463ac94eb39ee7f4007e540a3f8b
Guilherme H <guilherme.assis@luizalabs.com>
2019-11-16 14:10:09 -0300

    first commit

---
+++ src/main/java/br/com/unifacef/SpringProjectPostgresApplication.java
@@ -0,0 +1,13 @@
+package br.com.unifacef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringProjectPostgresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringProjectPostgresApplication.class, args);
+	}
+
+}
